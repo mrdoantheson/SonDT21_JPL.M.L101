07de064ca83f3d39d86f0090d18e3fb3330935dc
SonDT21 <mr.doantheson@gmail.com>
2023-02-18 17:57:44 +0700

    First commit

---
+++ src/fa/training/abstraction/Manager.java
@@ -0,0 +1,39 @@
+package fa.training.abstraction;
+
+import java.util.Scanner;
+
+public class Manager extends Employee {
+    private double wage;
+    private double basicSalary;
+
+    @Override
+    public double calcSalary() {
+        return wage * basicSalary;
+    }
+
+    @Override
+    protected void inputData(Scanner scanner) {
+        /**
+         * Call inputData method from parent class.
+         */
+        super.inputData(scanner);
+
+        System.out.println("Enter wage: ");
+        wage = scanner.nextDouble();
+
+        System.out.println("Enter basic salary: ");
+        basicSalary = scanner.nextDouble();
+
+        System.out.println("---------------------------");
+    }
+
+    @Override
+    protected void display() {
+        /**
+         * Call method of parent class
+         */
+        super.display();
+
+        System.out.print("\t" + wage + "\t" + basicSalary + "\t" + this.calcSalary() + "\n");
+    }
+}
